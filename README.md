330e4b4c0288e8356265f35b3f417bb490310dab
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-10-10 23:17:54 +0200

    added files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Wled_Ringer
 A Wled hotkey that changes presets like a doorbell 
+
+## How to use
+this is written for a wwmos d1 mini. 
+
+1. set ssid and pw
+2. set ip of your wled device in Line 33 and 35
+3. connect a button to pin 5 and gnd
+4. connect  a led to pin 5 and gnd
+5. done
+
+## 3D Print
+
+the STLs are for a phone charging station:
+
+![image](img.jpg)
---
+++ Wled_Ringer.ino
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+const int button = 5;
+const int duration = 8000;
+const int LED = 4;
+
+
+void setup() {
+
+  pinMode(LED, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+  
+  Serial.begin(115200);
+  delay(4000);
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("ssid", "pw");
+  Serial.print("Lets Go \n");
+}
+
+void loop() {
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    digitalWrite(LED,HIGH);
+    if (!digitalRead(button)){
+      digitalWrite(LED,LOW);
+      Serial.print("Butten Pressed \n");
+      send("http://192.168.178.68/win&&PL=2");
+      delay(duration);
+      send("http://192.168.178.68/win&&PL=1");
+      Serial.print("Animation done \n");
+    }   
+  }
+}
+
+void send(String msg){
+  WiFiClient client;
+  HTTPClient http;
+  //Serial.print("[HTTP] begin...\n"); 
+  if (http.begin(client, msg)) {
+    Serial.print("sending request \n");
+    int httpCode = http.GET();
+    http.end();
+  } else {
+    Serial.printf("[HTTP} Unable to connect\n");
+    }
+}
